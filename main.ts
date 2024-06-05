27dfca602ca8dbce620a5c6b5c4baca731a2a68f
ASNA <asnasiddiqui11@gmail.com>
2024-06-05 04:55:37 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers :{
+    numberone : number,
+    numbertwo : number,
+    operator : string
+} = await inquirer.prompt([
+
+{   name : "numberone",
+    type : "number",
+    message : (chalk.redBright)("Enter your first number")
+}, 
+{
+    name : "numbertwo",
+    type : "number",
+    message : (chalk.redBright)("Enter your second number")
+},    
+{
+    name : "operator",
+    type : "list",
+    message : (chalk.green)("Enter your operator"),
+    choices : ["+","-","*","/"]
+}
+]);
+
+let result
+switch (answers.operator) {
+    case "+":
+        result = (answers.numberone + answers.numbertwo);
+        break;
+    case "-":
+        result = (answers.numberone - answers.numbertwo);
+        break;
+    case "*":
+        result = (answers.numberone * answers.numbertwo);
+        break;
+    case "/":
+        result = (answers.numberone / answers.numbertwo);
+        break;
+    default:
+        console.log("invalid operator");
+        
+}
+
+console.log(chalk.yellow(`${answers.numberone} ${answers.operator} ${answers.numbertwo} = ${result}`));
